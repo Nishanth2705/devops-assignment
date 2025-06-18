5041fb158a172d5904161d2e8d07af1f473e5c4c
Nishanth2705 <nishanths0502@gmail.com>
2025-06-18 09:39:17 +0530

    Deleted all files from develop branch

--- frontend/__tests__/page.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from '../app/page';
-
-test('renders heading', () => {
-  render(<Home />);
-  expect(screen.getByText(/Frontend-Backend Integration/i)).toBeInTheDocument();
-});
